c5cf537ba773fe642353dfb99ffb5119067b9440
Junjia Chen <88954448+SeedJJ@users.noreply.github.com>
2026-06-07 23:13:53 +1000

    Add: lru-cache - submission-0

---
+++ Data Structures & Algorithms/lru-cache/submission-0.py
@@ -0,0 +1,68 @@
+class Node:
+    def __init__(self, key: int, value: int):
+        self.key = key
+        self.value = value
+        self.prev = None
+        self.next = None
+
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        self.cache = {}
+
+        self.left = Node(0, 0)
+        self.right = Node(0, 0)
+
+        self.left.next = self.right
+        self.right.prev = self.left
+
+    def remove(self, node: Node) -> None:
+        prev_node = node.prev
+        next_node = node.next
+
+        prev_node.next = next_node
+        next_node.prev = prev_node
+
+    def insert(self, node: Node) -> None:
+        prev_node = self.right.prev
+        next_node = self.right
+
+        prev_node.next = node
+        node.prev = prev_node
+
+        node.next = next_node
+        next_node.prev = node
+
+    def get(self, key: int) -> int:
+        if key not in self.cache:
+            return -1
+
+        node = self.cache[key]
+
+        self.remove(node)
+        self.insert(node)
+
+        return node.value
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.cache:
+            old_node = self.cache[key]
+            self.remove(old_node)
+
+        new_node = Node(key, value)
+        self.cache[key] = new_node
+        self.insert(new_node)
+
+        if len(self.cache) > self.capacity:
+            lru = self.left.next
+
+            self.remove(lru)
+            del self.cache[lru.key]
+        
+
+
+# Your LRUCache object will be instantiated and called as such:
+# obj = LRUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
